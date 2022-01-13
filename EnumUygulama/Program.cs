2404aaa4c46b65c2b8731601569b332fba78173e
Dilara <74327986+dilaraizibuyuk@users.noreply.github.com>
2022-01-13 18:53:32 +0300

    Proje dosyası ekle.

---
+++ EnumUygulama/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumUygulama
+{
+    class Program
+    {
+
+        enum bolumler
+        {
+            Bilgisayar_muhendisligi,
+            Elektrik_muhendisligi,
+            Makine_muhendisligi,
+            İnşaat_muhendisligi,
+
+        }
+        static void Main(string[] args)
+        {
+            bolumler b;
+            for(b=bolumler.Bilgisayar_muhendisligi; b <= bolumler.İnşaat_muhendisligi; b++)
+            {
+                Console.WriteLine("{0}'nın sayısı:{1}",b,(int)b);
+
+            }
+            Console.ReadKey();
+        }
+    }
+}
